cbfe3d449c0d7d9e89e88e93f371e255cc982371
ahmet kizilay <ahmet.kizilay@gmail.com>
2015-11-09 00:47:42 +0200

    code formatting

--- spec/components/hello.spec.js
+++ spec/components/hello.spec.js
@@ -9,6 +9,7 @@ import Hello from '../../src/components/hello.jsx';
 describe('Hello Component', () => {
   it('should have the right text', () => {
     const content = "hi";
+
     var component = TestUtils.renderIntoDocument(<Hello name={content}/>);
 
     var helloNode = ReactDOM.findDOMNode(component);
