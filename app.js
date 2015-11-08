a79f9959c25b7bcc2f32389b8f2b19080e9e5254
ahmet kizilay <ahmet.kizilay@gmail.com>
2015-11-08 14:20:45 +0200

    initialized

---
+++ app.js
@@ -0,0 +1,33 @@
+require('babel-core/register');
+'use strict';
+
+var fs = require('fs');
+var koa = require('koa');
+var router = require('koa-router')();
+var serve = require('koa-static');
+var mount = require('koa-mount');
+
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+
+var _ = require('lodash');
+
+var App = require('./src/index.js');
+
+var template = fs.readFileSync("./template.html");
+
+var app = koa();
+router.get("/", function* () {
+  var rendered = ReactDOMServer.renderToString(React.createElement(App));
+  this.body = _.template(template)({ body: rendered });
+});
+
+app.use(mount("/dist", serve("./dist")));
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000);
+
+
+
---
+++ src/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Hello from './components/hello.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: 'Ahmet'
+    }
+  }
+
+  render() {
+    return (<Hello name={this.state.name} />)
+  }
+}
+
+module.exports = App;
